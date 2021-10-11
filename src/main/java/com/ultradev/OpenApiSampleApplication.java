6da38659c1c2516542202e3d78a22f7a3a6b5813
ultra-devs <ultradev.nitc@gmail.com>
2021-10-11 16:59:46 -0400

    Added mvn server files

---
+++ src/main/java/com/ultradev/OpenApiSampleApplication.java
@@ -0,0 +1,13 @@
+package com.ultradev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ultradev/OpenApiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultradev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
